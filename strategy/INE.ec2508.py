f1ad0d9a8a7736208b538a7f28f14369bc015dc3
Atlantis <atlantisspeed@gmail.com>
2025-03-06 19:19:19 +0800

    push 脚本

--- strategy/INE.ec2508.py
+++ strategy/INE.ec2508.py
@@ -25,7 +25,8 @@ elif cfg.tq_kq:
     api = TqApi(TqKq(), auth=auth)
 elif cfg.tq_back_test:
     now = datetime.datetime.now()
-    api = TqApi(backtest=TqBacktest(start_dt=date(2024, 8, 20), end_dt=date(now.year, now.month, now.day)), web_gui=True, auth=auth)
+    api = TqApi(backtest=TqBacktest(start_dt=date(2024, 8, 20), end_dt=date(now.year, now.month, now.day)),
+                web_gui=True, auth=auth)
 else:
     api = TqApi(TqKq(), auth=auth)
 
@@ -41,6 +42,7 @@ k_m1 = api.get_kline_serial(symbol, Kline.MINUTE1.value, data_length=15)
 k_s10 = api.get_kline_serial(symbol, Kline.SECONDS10.value, data_length=15)
 k_s15 = api.get_kline_serial(symbol, Kline.SECONDS15.value, data_length=15)
 k_s30 = api.get_kline_serial(symbol, Kline.SECONDS30.value, data_length=15)
+k_s5 = api.get_kline_serial(symbol, Kline.SECONDS5.value, data_length=15)
 
 macd_day = MACD(k_day, 12, 26, 9)
 macd_m30 = MACD(k_m30, 12, 26, 9)
@@ -54,7 +56,7 @@ position = api.get_position(symbol)
 target_pos = TargetPosTask(api, symbol)
 
 ls = api.query_cont_quotes()
-open_position_amount = 10
+open_position_amount = 2
 
 if __name__ == '__main__':
     print(f"开仓数量 {open_position_amount}")
@@ -76,14 +78,15 @@ if __name__ == '__main__':
             k_line_h2 = k_h2.iloc[-1]
             print(f"30分钟线 K线起始时刻的最新价：{k_line_m30.open} K线结束时刻的最新价：{k_line_m30.close}")
 
-            status_m30 = k_line_status(k_line_m30.close, k_line_m30.open)
-            status_h1 = k_line_status(k_line_h1.close, k_line_h1.open)
-            status_day = k_line_status(k_line_day.close, k_line_day.open)
+            status_m30 = k_line_status(last_price, k_line_m30.open)
+            status_h1 = k_line_status(last_price, k_line_h1.open)
+            status_day = k_line_status(last_price, k_line_day.open)
             print(f"30分钟线状态：{status_m30}")
             print(f"1小时线状态：{status_h1}")
             print(f"日线状态：{status_day}")
 
-            status = k_line_status(last_price, k_line_h2.open)
+            status_h2 = k_line_status(last_price, k_line_h2.open)
+            status = status_day
             # status = k_line_status(last_price, k_line_day.open)
             print(f"status：{status}")
 
