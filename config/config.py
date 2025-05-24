dbef61e5b3567372f9afd9da6bf0dc5fc66c1a17
Atlantis <atlantisspeed@gmail.com>
2025-05-24 14:32:03 +0800

    iniconfig 使用

---
+++ config/config.py
@@ -0,0 +1,14 @@
+from iniconfig import IniConfig
+
+ini = IniConfig('../config_test.ini')
+
+db_host = ini.get('Database', 'host')
+
+print(f"Database host {db_host}")
+db_host_1 = ini['Database']['host']
+print(f"Database host {db_host_1}")
+
+for key in ini.sections:
+    print(f"sections key={key} keys={list(ini[key])}")
+    for value in list(ini[key]):
+        print(f"sections key={key} {value}={ini[key][value]}")
