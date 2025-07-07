b445c51502eea0746d2da05ccfc2fbdeb3902531
Atlantis <AtlantisSpeed@gmail.com>
2025-07-07 23:10:49 +0800

    纯苯期货

--- configs/config.py
+++ configs/config.py
@@ -12,3 +12,8 @@ for key in ini.sections:
     print(f"sections key={key} keys={list(ini[key])}")
     for value in list(ini[key]):
         print(f"sections key={key} {value}={ini[key][value]}")
+
+class Configs(object):
+    def init_config(self):
+        ini = IniConfig('../config_test.ini')
+        db_host = ini.get('Database', 'host')
